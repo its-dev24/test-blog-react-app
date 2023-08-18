8a79347e57a8cd6008ecb38d626d4c0e283ee577
Gautham <gauthamskaniyattil@gmail.com>
2023-08-18 11:37:35 +0530

    Started working on the css

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,11 +1,12 @@
-import "./App.css";
 
+import Navbar from "./Navbar";
+import Home from "./Home";
 function App() {
-  let title = "Welcome to the new blog";
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
+        <Home />
       </div>
     </div>
   );
