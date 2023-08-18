c9271f6d9b82aff986526f15b204c2b7c81e8bad
Gautham <gauthamskaniyattil@gmail.com>
2023-08-18 23:46:09 +0530

    Added Blog preview

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,18 +1,31 @@
 import Button from "@mui/material/Button";
 import { useState } from "react";
 const Home = () => {
-  const [name, setName] = useState("jadhu");
-
-  const changeDetails = (name) => {
-    setName(name);
-  };
+  const [Blogs, setBlogs] = useState([
+    {
+      title: "First Day in College",
+      body: "Temdsjfljadsjfoodsofdsfhdshf sdhfhdshfhasodhfohdsfkjsandf",
+      author: "Mario",
+    },
+    {
+      title: "Work Blog",
+      body: "Temdsjfljadsjfoodsofdsfhdshf sdhfhdshfhasodhfohdsfkjsandf",
+      author: "Teacher",
+    },
+    {
+      title: "Tired Day",
+      body: "Temdsjfljadsjfoodsofdsfhdshf sdhfhdshfhasodhfohdsfkjsandf",
+      author: "Ann",
+    },
+  ]);
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <h3>{name} is an MCA Student</h3>
-      <Button variant="contained" onClick={() => changeDetails("Shijin")}>
-        Click
-      </Button>
+      {Blogs.map((blog) => (
+        <div className="blog-preview">
+          <h2>{blog.title}</h2>
+          <p>Written By {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
--- dojo-blog/src/Navbar.js
+++ dojo-blog/src/Navbar.js
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>welcome to dojo blog</h1>
+      <h1>E-Blog</h1>
       <div className="links">
         <a href="/">Home</a>
         <a
@@ -12,7 +12,7 @@ const Navbar = () => {
             borderRadius: "6px",
           }}
         >
-          Create
+          New Blog
         </a>
       </div>
     </nav>
