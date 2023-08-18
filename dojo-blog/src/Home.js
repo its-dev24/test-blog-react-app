fd40fd136ad1c36453af4dab974368b33cf2a898
Gautham <gauthamskaniyattil@gmail.com>
2023-08-18 13:18:37 +0530

    Added hooks

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,18 @@
+import Button from "@mui/material/Button";
+import { useState } from "react";
 const Home = () => {
+  const [name, setName] = useState("jadhu");
+
+  const changeDetails = (name) => {
+    setName(name);
+  };
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <h3>{name} is an MCA Student</h3>
+      <Button variant="contained" onClick={() => changeDetails("Shijin")}>
+        Click
+      </Button>
     </div>
   );
 };
