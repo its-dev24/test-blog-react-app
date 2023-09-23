c3e153b418aec1285c3b26d7a90ff0b06534e8cb
Gautham <gauthamskaniyattil@gmail.com>
2023-09-23 22:56:13 +0530

    add blog page layout completed

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,7 +1,36 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [content, setContent] = useState("");
   return (
     <div className="create">
       <h1>Add New Blog</h1>
+      <form>
+        <label>Title</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Author</label>
+        <select required>
+          <option value="" selected>
+            Author
+          </option>
+          <option value="Mario">Mario</option>
+          <option value="luigi">Luigi</option>
+        </select>
+        <label>Content</label>
+        <textarea
+          required
+          value={content}
+          onChange={(e) => setAuthor(e.target.value)}
+        ></textarea>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 };
