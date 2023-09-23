0ee54f8ec08ea3dc71e74fa08222941e2fb4c7de
Gautham <gauthamskaniyattil@gmail.com>
2023-09-24 00:08:13 +0530

    Finished add blogs functionality

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -2,12 +2,27 @@ import { useState } from "react";
 
 const Create = () => {
   const [title, setTitle] = useState("");
-  const [author, setAuthor] = useState("");
-  const [content, setContent] = useState("");
+  const [author, setAuthor] = useState("Mario");
+  const [body, setBody] = useState("");
+  const [isPending, setIsPending] = useState(false);
+
+  const handleSubmit = (e) => {
+    setIsPending(true);
+    e.preventDefault();
+    const blog = { title, author, body };
+    fetch("http://localhost:8000/blogs", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(blog),
+    }).then(console.log("Blog added"));
+    setIsPending(false);
+  };
   return (
     <div className="create">
       <h1>Add New Blog</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Title</label>
         <input
           type="text"
@@ -16,20 +31,22 @@ const Create = () => {
           onChange={(e) => setTitle(e.target.value)}
         />
         <label>Author</label>
-        <select required>
-          <option value="" selected>
-            Author
-          </option>
+        <select
+          required
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
           <option value="Mario">Mario</option>
           <option value="luigi">Luigi</option>
         </select>
         <label>Content</label>
         <textarea
           required
-          value={content}
-          onChange={(e) => setAuthor(e.target.value)}
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
         ></textarea>
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding Blog...</button>}
       </form>
     </div>
   );
